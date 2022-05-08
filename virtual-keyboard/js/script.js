85d0348d2e0f8cf7a43adf02119405b08497c82c
Egor Shkel <skelegor8@gmail.com>
2022-05-08 20:01:12 +0300

    feat: add readme

--- virtual-keyboard/js/script.js
+++ virtual-keyboard/js/script.js
@@ -16,6 +16,7 @@ sectionTextArea.prepend(divContainer);
 
 let textarea = document.createElement('textarea');
 textarea.className = "textarea";
+textarea.setAttribute("autofocus", "autofocus")
 divContainer.prepend(textarea);
 
 let sectionKeyboard = document.createElement('section');
@@ -91,7 +92,7 @@ str4.className = "str";
 str4.classList.add('str-4');
 divKeyboardContainer.append(str4);
 
-addBtn(str4, true, keys[42], language, "shift");
+addBtn(str4, true, keys[42], language, "shift-right");
 addBtn(str4, false, keys[43], language);
 addBtn(str4, false, keys[44], language);
 addBtn(str4, false, keys[45], language);
@@ -102,23 +103,23 @@ addBtn(str4, false, keys[49], language);
 addBtn(str4, false, keys[50], language);
 addBtn(str4, false, keys[51], language);
 addBtn(str4, false, keys[52], language);
-addBtn(str4, false, keys[53], language);
-addBtn(str4, true, keys[54], language);
+addBtn(str4, false, keys[53], language, "up");
+addBtn(str4, true, keys[54], language, "shift-left");
 
 
 let str5 = document.createElement('div');
 str5.className = "str";
 str5.classList.add('str-5');
 divKeyboardContainer.append(str5);
-addBtn(str5, true, keys[55], language);
-addBtn(str5, true, keys[56], language);
+addBtn(str5, true, keys[55], language, "ctrl-right");
+addBtn(str5, true, keys[56], language, "alt-right");
 addBtn(str5, true, keys[57], language);
 addBtn(str5, true, keys[58], language, "space");
-addBtn(str5, true, keys[59], language);
-addBtn(str5, true, keys[60], language);
-addBtn(str5, false, keys[61], language);
-addBtn(str5, false, keys[62], language);
-addBtn(str5, false, keys[63], language);
+addBtn(str5, true, keys[59], language, "alt-left");
+addBtn(str5, true, keys[60], language, "ctrl-left");
+addBtn(str5, false, keys[61], language, "left");
+addBtn(str5, false, keys[62], language, "down");
+addBtn(str5, false, keys[63], language, "right");
 
 
 function addBtn(str, fn, id, lang, more) {
@@ -128,15 +129,36 @@ function addBtn(str, fn, id, lang, more) {
     btn.classList.add('fn-btn');
     if (more == "space") {
       btn.classList.add('space');
-    } else if (more == "shift") {
-      btn.classList.add('shift');
+    } else if (more == "shift-right") {
+      btn.classList.add('shift-right');
+    } else if (more == "shift-left") {
+      btn.classList.add('shift-left');
     } else if (more == "enter") {
       btn.classList.add('enter');
     } else if (more == "caps-lock") {
       btn.classList.add('caps-lock');
     } else if (more == "tab") {
       btn.classList.add('tab');
+    } else if (more == "ctrl-right") {
+      btn.classList.add('ctrl-right');
+    } else if (more == "ctrl-left") {
+      btn.classList.add('ctrl-left');
+    } else if (more == "alt-right") {
+      btn.classList.add('alt-right');
+    } else if (more == "alt-left") {
+      btn.classList.add('alt-left');
     }
+  }
+  if (more == "up") {
+    btn.classList.add("up");
+  } else if (more == "down") {
+    btn.classList.add("down");
+  }
+  else if (more == "right") {
+    btn.classList.add("right");
+  }
+  else if (more == "left") {
+    btn.classList.add("left");
   }
   str.append(btn);
 
